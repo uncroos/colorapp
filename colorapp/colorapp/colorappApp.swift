4fbdf61c3deb44c32a7292ce56160fb1f7b58e8a
uncroos <any492266@gmail.com>
2024-09-30 23:52:00 +0900

    시작하자~ 🚀

---
+++ colorapp/colorapp/colorappApp.swift
@@ -0,0 +1,17 @@
+//
+//  colorappApp.swift
+//  colorapp
+//
+//  Created by 안요한 on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct colorappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
