c32cd4e5e26a52bccad9cba5637e61f479beaa57
uncroos <any492266@gmail.com>
2024-10-01 00:23:42 +0900

    Splash 화면 작성

--- colorapp/colorapp/SplashScreen.swift
+++ colorapp/colorapp/SplashScreen.swift
@@ -1,8 +1,38 @@
-//
-//  SplashScreen.swift
-//  colorapp
-//
-//  Created by 안요한 on 10/1/24.
-//
+import SwiftUI
 
-import Foundation
+struct SplashScreen: View {
+    @State private var isActive = false  // 스플래시 화면 활성화 상태 변수
+
+    var body: some View {
+        if isActive {
+            // 스플래시가 끝난 후 나타나는 ContentView
+            ContentView()
+        } else {
+            // 스플래시 화면
+            VStack {
+                Image("logo")  // Assets에 추가한 logo.png 파일을 불러옵니다.
+                    .resizable()  // 이미지 크기 조정 가능
+                    .aspectRatio(contentMode: .fit)  // 비율에 맞게 크기 조정
+                    .frame(width: 200, height: 200)  // 이미지 크기 설정
+                    
+            }
+            .onAppear {
+                // 스플래시 화면을 2초 후에 종료
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation {
+                        isActive = true  // 스플래시 화면 종료
+                    }
+                }
+            }
+            .background(Color.white)  // 스플래시 화면 배경색 설정
+            .edgesIgnoringSafeArea(.all)  // 안전 영역 무시
+        }
+    }
+}
+
+struct SplashScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreen()  // SplashScreen 미리보기
+    }
+}
+
--- colorapp/colorapp/colorappApp.swift
+++ colorapp/colorapp/colorappApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct colorappApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashScreen()
         }
     }
 }
